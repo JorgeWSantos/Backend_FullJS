1d2edbc4fc06c1c660faa89b30e4e7e4705cf9b3
Jorge <jorgeewillian00@hotmail.com>
2019-10-04 02:31:49 -0300

    adicionando dashboard(retorna um filtro de spots), adicionado booking(faz reserva de spots)

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -11,7 +13,13 @@ const upload = multer(uploadConfig);
 //req.params. -- acessa os atributos pela rota ''/users/:id''
 //req.body -- acessar o corpo da requisição
 routes.post('/sessions', SessionController.store)
+
 routes.post('/spots', upload.single('thumbnail'),SpotController.store)
 routes.get('/spots',SpotController.index)
+
+routes.get('/dashboard', DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
+
 
 module.exports = routes;
