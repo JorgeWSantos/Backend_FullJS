2289112b9dada6111243cc3adcf72218ee7a3640
Jorge <jorgeewillian00@hotmail.com>
2019-10-04 01:52:33 -0300

    adicionando o modelo spot da aplicação, e adicionando duas funcionalidades, cadastro de spot e listagem por tech, a dependecia multer foi adicionada para interagir com arquivos enviados

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
+
 //req.query.params. -- acessa os atributos query
 //req.params. -- acessa os atributos pela rota ''/users/:id''
 //req.body -- acessar o corpo da requisição
 routes.post('/sessions', SessionController.store)
+routes.post('/spots', upload.single('thumbnail'),SpotController.store)
+routes.get('/spots',SpotController.index)
 
 module.exports = routes;
